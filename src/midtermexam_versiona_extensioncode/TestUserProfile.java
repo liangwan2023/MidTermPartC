4a50794a4523fd26bcac856a2ba156a7a7cf9b17
Wanqin liang <liangwan@sheridancollege.ca>
2023-06-20 14:16:21 -0400

    Add user profile class

---
+++ src/midtermexam_versiona_extensioncode/TestUserProfile.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author tingman
+ */
+public class TestUserProfile {
+    private String Name;
+    private String Genre;
+    public TestUserProfile();
+    public void String toString(){
+        return userProfile was created;
+    }
+    
+    
+}
